c561472babf4f9b65984fd7835fdb7c99fa9d39c
Maga002 <gulalovm777@gmail.com>
2021-06-30 10:41:34 +0400

    WebSuper

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+ $(".all-slider").slick({
+      arrows:false,
+      autoplay: true,
+      autoplaySpeed: 3000
+  
+      
+   })
+   function openNav() {
+      document.getElementById("mySidenav").style.width = "300px";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    } 
